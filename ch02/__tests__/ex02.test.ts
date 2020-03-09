33a9f33081608bf5bf5ecbde4e14bf826bad13d2
naoya.kitoh <naoya.kitoh@nts.ricoh.co.jp>
2020-03-09 21:22:26 +0900

    add test ex02

--- ch02/__tests__/ex02.test.ts
+++ ch02/__tests__/ex02.test.ts
@@ -4,3 +4,11 @@ test('slow speed', () => {
   expect(getSpeed("slow")).toEqual(10);
 });
 
+test('medium speed', () => {
+  expect(getSpeed("medium")).toEqual(10);
+});
+
+test('fast speed', () => {
+  expect(getSpeed("slow")).toEqual(10);
+});
+
